c35ce908e270466b7205680c97e7c3c64bae6883
Ian <ianburrows.sa@gmail.com>
2017-09-18 13:05:26 +0200

    node add

---
+++ CodingDojo02Node/NodeStack/NodeElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo02Node.NodeStack
+{
+    class NodeElement<T>
+    {
+        public NodeElement<T> NextNode { get; set; }
+        public T ValueOfNode { get; set; }
+    }
+}
---
+++ CodingDojo02Node/NodeStack/NodeStack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo02Node.NodeStack
+{
+    class NodeStack<T>
+    {
+        private NodeElement<T> currentNode;
+
+        public void Push(T item)
+        {
+            if (currentNode == null)
+            {
+                currentNode = new NodeElement<T> { ValueOfNode = item, NextNode = null };
+            }
+            else
+            {
+                NodeElement<T> temp = new NodeElement<T> { ValueOfNode = item, NextNode = currentNode };
+                currentNode = temp;
+            }
+        }
+    }
+}
